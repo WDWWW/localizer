187c7702a8fb88eed5c4b642804191982a0cf096
Enif Lee <enif.lee@gmail.com>
2021-01-12 04:58:34 +0900

    nsubstitute usage test

---
+++ server/Localizer.Test/Libraries/NSubstituteUsageTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using Xunit;
+
+namespace Localizer.Test.Libraries
+{
+    public class NSubstituteUsageTest
+    {
+        private ISomethingProvider _somethingProvider;
+
+        public interface ISomethingProvider
+        {
+            public string SomethingNow { get; set; }
+
+            public string GetSomething();
+
+            public Task<string> GetSomethingAsync();
+
+            public void SetSomething(string value);
+        }
+
+        public NSubstituteUsageTest()
+        {
+            _somethingProvider = Substitute.For<ISomethingProvider>();
+        }
+
+        [Fact]
+        public void MockPropertyTest()
+        {
+            // Given
+            _somethingProvider.SomethingNow.Returns("hello");
+            
+            // When
+            var result = _somethingProvider.SomethingNow;
+            
+            // Then
+            result.Should().Be("hello");
+            var _ = _somethingProvider.Received().SomethingNow; // property verifying call count
+        }
+
+        [Fact]
+        public void MockMethod()
+        {
+            // Given
+            _somethingProvider.GetSomething().Returns("hello_method_call");
+            
+            // When
+            var result = _somethingProvider.GetSomething();
+            
+            // Then
+            result.Should().Be("hello_method_call");
+            _somethingProvider.Received().GetSomething();
+        }
+
+        [Fact]
+        public async Task MockAsyncMethod()
+        {
+            // Given
+            _somethingProvider.GetSomethingAsync().Returns("hello_async_method_call");
+            
+            // When
+            var result = await _somethingProvider.GetSomethingAsync();
+            
+            // Then
+            result.Should().Be("hello_async_method_call");
+            await _somethingProvider.Received().GetSomethingAsync();
+        }
+    }
+}
