84d7872d6146a12246c037455878c42259dfee0b
Enif Lee <enif.lee@gmail.com>
2021-01-14 03:01:42 +0900

    add repository base

---
+++ server/Localizer.Domain/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Localizer.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace Localizer.Domain.Infrastructure
+{
+	public abstract class RepositoryBase<TEntity, TKey> : IDisposable, IAsyncDisposable where TEntity : class, IEntityKey<TKey> where TKey : struct
+	{
+		private readonly LocalizerDb _db;
+
+		protected RepositoryBase(LocalizerDb db)
+		{
+			_db = db;
+		}
+
+		protected IQueryable<TEntity> Query => _db.Set<TEntity>().AsQueryable();
+
+		public Task<bool> AnyIdAsync(TKey key) => Query.AnyAsync(entity => entity.Id.Equals(key));
+
+		public int SaveChanges() => _db.SaveChanges();
+
+		public Task<int> SaveChangesAsync(CancellationToken cancellationToken = new()) => _db.SaveChangesAsync(cancellationToken);
+		public async Task UpdateAsync(TKey key, Action<TEntity> updater)
+		{
+			var entity = await _db.FindAsync<TEntity>(key);
+			updater(entity);
+			await _db.SaveChangesAsync();
+		} 
+
+		public async Task RemoveAsync(TKey key)
+		{
+			var entity = await _db.FindAsync<TEntity>(key);
+			_db.Remove(entity);
+			await _db.SaveChangesAsync();
+		}
+		
+		public TEntity Find(TKey key) => _db.Find<TEntity>(key);
+
+		public ValueTask<TEntity> FindAsync(TKey key) => _db.FindAsync<TEntity>(key);
+
+		public void Dispose()
+		{
+			_db?.Dispose();
+		}
+
+		public ValueTask DisposeAsync() => _db.DisposeAsync();
+	}
+}
