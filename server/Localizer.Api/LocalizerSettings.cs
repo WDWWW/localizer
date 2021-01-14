d8653360d84e3bd8bfbdcf22660b9205e81aa9b9
Enif Lee <enif.lee@gmail.com>
2021-01-14 10:46:52 +0900

    configure db context settings for api server with adding database connection parameter

--- server/Localizer.Api/LocalizerSettings.cs
+++ server/Localizer.Api/LocalizerSettings.cs
@@ -1,18 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable UnusedMember.Global
+
 namespace Localizer.Api
 {
 	public class LocalizerSettings
 	{
+		/// <summary>
+		///		Localizer authentication configuration
+		/// </summary>
 		[Required]
 		public AuthenticationSecretSettings SecretSettings { get; set; } = new();
+
+		/// <summary>
+		///		Localizer database connection string. only allowed for postgresql.
+		/// </summary>
+		[Required]
+		public string DatabaseConnection { get; set; } = string.Empty;
 	}
 
 	public class AuthenticationSecretSettings
 	{
+		/// <summary>
+		///		System default name for 
+		/// </summary>
 		[Required]
 		public string ServiceName { get; set; } = string.Empty;
 
+		/// <summary>
+		///		Token signing key for generating jwt key as access token.
+		/// </summary>
 		[Required]
 		public string TokenSigningKey { get; set; } = string.Empty;
 	}
--- server/Localizer.Api/Startup.cs
+++ server/Localizer.Api/Startup.cs
@@ -1,33 +1,49 @@
 using System.Text;
 using Localizer.Api.Repositories;
 using Localizer.Api.Services;
+using Localizer.Common;
+using Localizer.Domain;
+using Localizer.Domain.Infrastructure;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Localizer.Api
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
+		private readonly IWebHostEnvironment _environment;
+
+		public Startup(IConfiguration configuration, IWebHostEnvironment environment)
 		{
+			_environment = environment;
 			Configuration = configuration;
 		}
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddControllers();
 
+			services.AddSingleton<IDateTimeOffsetProvider, UtcDateTimeOffsetProvider>();
+			services.AddOptions<LocalizerSettings>()
+				.Bind(Configuration.GetSection("LocalizerSettings"))
+				.ValidateDataAnnotations()
+				.Validate((LocalizerSettings _, LocalizerDb db) =>
+				{
+					db.Database.OpenConnection();
+					return true;
+				});
+			services.AddScoped(provider => provider.GetRequiredService<IOptionsSnapshot<LocalizerSettings>>().Value);
 			services.AddScoped<AccountRepository>();
 			services.AddScoped<AuthenticationService>();
-
 			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 				.AddJwtBearer(options =>
 				{
@@ -42,11 +58,20 @@ namespace Localizer.Api
 						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("localizer_demo_secret_key")),
 					};
 				});
+			
+			services.AddDbContext<LocalizerDb>();
+			services.AddScoped(provider =>
+			{
+				var settings = provider.GetRequiredService<LocalizerSettings>();
+				return new DbContextOptionsBuilder<LocalizerDb>()
+					.UseLazyLoadingProxies()
+					.UseNpgsql(settings.DatabaseConnection)
+					.AddInterceptors(new WriteTimeInterceptor(provider.GetRequiredService<IDateTimeOffsetProvider>()))
+					.Options;
+			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app,
-			IWebHostEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 				app.UseDeveloperExceptionPage();
